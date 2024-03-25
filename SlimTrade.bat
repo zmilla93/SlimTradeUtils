47e8995738435db9913c76cd843198568e4554be
Zach Miller <zmilla93@gmail.com>
2024-03-25 15:41:06 -0500

    init

---
+++ SlimTrade.bat
@@ -0,0 +1,18 @@
+:: This file will run SlimTrade as an administrator.
+
+@echo off
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+if '%errorlevel%' NEQ '0' (goto UACPrompt) else ( goto gotAdmin )
+:UACPrompt
+echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+echo UAC.ShellExecute "%~s0", "", "", "runas", 0 >> "%temp%\getadmin.vbs"
+"%temp%\getadmin.vbs"
+exit /B
+:gotAdmin
+if exist "%temp%\getadmin.vbs" ( del "%temp%\getadmin.vbs" )
+pushd "%CD%"
+CD /D "%~dp0"
+
+:: If this .bat file is in a different folder than SlimTrade.jar,
+:: use the full path to the .jar file in the next line.
+java -jar "SlimTrade.jar"
